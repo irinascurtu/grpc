67c2530bc5bf5e4f010003afc275108ef893fc14
Irina Scurtu <scurtuirina@gmail.com>
2019-07-23 13:35:35 +0300

    fibonacci sample project

---
+++ MyFirstGrpc/MyFirstGrpc.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Grpc.Core;
+
+namespace MyFirstGrpc.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Fibo.FiboClient(channel);
+            while (true)
+            {
+                String number = Console.ReadLine();
+                int n = Int32.Parse(number);
+                var reply = client.ComputeFibonacci(new RequestedNumber { Number = n });
+                Console.WriteLine($"Fibonacci for {n} is:{reply.Result}");
+            }
+        }
+    }
+}
---
+++ MyFirstGrpc/MyFirstGrpc/Services/FibonacciService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace MyFirstGrpc.Services
+{
+    public class FibonacciService : Fibo.FiboBase
+    {
+        public override Task<FibonacciResult> ComputeFibonacci(RequestedNumber request, ServerCallContext context)
+        {
+            int n = request.Number;
+
+            return Task.FromResult(new FibonacciResult
+            {
+                Result = Fibonacci(n)
+            });
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+    }
+}
